cd325042b012e5c1195776e83721e9d4238bad36
Sergii Podriezienko <podreenko1986@gmail.com>
2023-03-06 11:27:59 +0200

    id value
    id value

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+class Form extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <form>
+        <label>
+          Name
+          <input
+            id={nanoid()}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default Form;
